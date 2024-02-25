451006a41ba7d773c60c8d926414700e8187720a
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2024-02-25 17:06:40 +1000

    todo: make plugin group

---
+++ examples/visuals.rs
@@ -0,0 +1,39 @@
+use bevy::prelude::*;
+use bevy_hanabi::HanabiPlugin;
+use bevy_xpbd_3d_thrusters::prelude::*;
+use bevy_editor_pls::prelude::*;
+
+fn main() {
+	let mut app = App::new();
+
+	app.add_plugins((DefaultPlugins, HanabiPlugin, EditorPlugin::default()));
+
+	app.add_systems(Startup, setup);
+
+	app.run();
+}
+
+fn setup(
+	mut commands: Commands,
+	mut mats: ResMut<Assets<StandardMaterial>>,
+	mut meshs: ResMut<Assets<Mesh>>,
+) {
+	commands.spawn(Camera3dBundle {
+		transform: Transform::from_xyz(0.0, 0.0, 10.),
+		..default()
+	});
+	commands.insert_resource(AmbientLight {
+		color: Color::WHITE,
+		brightness: 60.0,
+	});
+
+	commands.spawn((
+		Thruster { strength: 1.0 },
+		PbrBundle {
+			material: mats.add(Color::GREEN),
+			mesh: meshs.add(Cuboid::from_size(Vec3::splat(1.0))),
+			transform: Transform::from_xyz(0.0, 0.0, 0.0),
+			..default()
+		},
+	));
+}
